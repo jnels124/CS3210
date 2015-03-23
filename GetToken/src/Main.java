62d323f4451090b355e61729b4913fb8507609e1
Jesse <jnels124@msudenver.edu>
2015-03-23 15:09:10 -0600

    Cleaned up logic

--- GetToken/src/Main.java
+++ GetToken/src/Main.java
@@ -30,7 +30,7 @@ public class Main {
     private static final Map<String, Integer> reservedTokens = new HashMap<String, Integer>();
     private static final Map<String, Integer> operatorSeperatorTokens = new HashMap<String, Integer>();
 
-    private static  String operatorsSeperatorsRegEx = "([-+*/()<>=;\n]|:=| )";
+    private static  String operatorsSeperatorsRegEx = "([-+*/()<>=;\n.]|:=|[0-9]| )";
     // Inititalize token map
     static {
         // Reserved words
@@ -61,19 +61,17 @@ public class Main {
         operatorSeperatorTokens.put("<", 23);
         operatorSeperatorTokens.put(":=", 24);
         operatorSeperatorTokens.put(";", 25);
-
-//        tokenMap.put("SPACE", 26);
-//        tokenMap.put("EOLN", 12);
-//        tokenMap.put("IDENTIFIER", 12);
-//        tokenMap.put("number", 29);
-//        tokenMap.put("string", 30);
-//        tokenMap.put("PERIOD", 31);
+        operatorSeperatorTokens.put(" ", 26);
+        operatorSeperatorTokens.put("\n", 27);
+        operatorSeperatorTokens.put(".", 26);
     }
-    static PushbackReader in;
 
-    public static void main (String args []) {
+    private static PushbackReader in;
+
+    public static void main (String args []) throws Exception{
 
         if (args.length == 1) {
+            // Handle Windows file system
             String pathToFile = System.getProperty("user.dir").replace("\\", "/");
             pathToFile += "/" + args[0];
             try {
@@ -83,153 +81,140 @@ public class Main {
             }
         }
         in = new PushbackReader(new InputStreamReader(System.in));
-        getToken();
+        char currChar;
+        // '\uFFFF' is returned when end of file is read
+        while ((currChar = readIn()) != '\uFFFF') {
+            getToken(currChar);
+        }
     }
 
-    public static void getToken() {
-        try {
-            char currChar = (char)in.read();
-            char nextChar;
-            switch (currChar) {
-                case ')':
-                    System.out.println("Token: " + ')' + " Value: " +  12);
-                    getToken();
-                    break;
-                case '(':
-                    System.out.println("Token: " + '(' + " Value: " +  13);
-                    getToken();
-                    break;
-                case '/':
-                    System.out.println("Token: " + '/' + " Value: " +  14);
-                    getToken();
-                    break;
-                case '*':
-                    System.out.println("Token: " + '*' + " Value: " +  15);
-                    getToken();
-                    break;
-                case '-':
-                    System.out.println("Token: " + ')' + " Value: " +  16);
-                    getToken();
-                    break;
-                case '+':
-                    System.out.println("Token: " + '+' + " Value: " +  17);
-                    getToken();
-                    break;
-                case '=':
-                    System.out.println("Token: " + '=' + " Value: " +  21);
-                    getToken();
-                    break;
-                case '>':
-                    nextChar = (char)in.read();
-                    if (nextChar == '=') {
-                        System.out.println("Token: " + ">=" + " Value: " +  20);
-                    }
-                    else {
-                        System.out.println("Token: " + ">" + " Value: " +  19);
-                        in.unread(nextChar);
-                    }
-                    getToken();
-                    break;
-                case '<':
-                    nextChar = (char)in.read();
-                    if (nextChar == '>') {
-                        System.out.println("Token: " + "<>" + " Value: " +  18);
-                    }
-                    else if (nextChar == '='){
-                        System.out.println("Token: " + "<=" + " Value: " +  22);
-                    }
-                    else {
-                        System.out.println("Token: " + "<" + " Value: " +  23);
-                        in.unread(nextChar);
-                    }
-                    getToken();
-                    break;
-                case ';':
-                    System.out.println("Token: " + ';' + " Value: " +  25);
-                    getToken();
-                    break;
-                case ' ':
-                    System.out.println("Token: " + "SPACE" + " Value: " +  26);
-                    getToken();
-                    break;
-                case '\n':
-                    System.out.println("Token: " + "EOLN" + " Value: " +  27);
-                    getToken();
-                    break;
-                case QUOTE:
-                    String strInput ="\"";
-                    char c;
-                    do {
-                        c = (char)in.read();
-                        strInput += c;
-                    } while (c != QUOTE);
-                    System.out.println("Token: " + "STRING--> " + strInput + " Value: " +  30);
-                    getToken();
-                    break;
-                case '.':
-                    nextChar = (char)in.read();
-                    if (isNumber(nextChar)) {
-                        throw new InputMismatchException("White space is required between PERIOD and NUMBER");
-                    }
-                    else {
-                        in.unread(nextChar);
-                        System.out.println("Token: " + "PERIOD" + " Value: " +  31);
-                        getToken();
-                        break;
-                    }
-                case ':':
-                    nextChar = (char)in.read();
-                    if (nextChar == '=') {
-                        System.out.println("Token: " + ":=" + " Value: " +  24);
-                        getToken();
-                        break;
-                    }
-                    else {
-                        in.unread(nextChar);
-                        // Now fall through to handle as part of an identifier
-                    }
-                default:
-                    if (isNumber(currChar)) {
-                        String number = "" + currChar;
-                        nextChar = (char)in.read();
-                        while (nextChar >= '0' && nextChar <= '9') {
-                            number += nextChar;
-                            nextChar = (char)in.read();
-                        }
-                        if (nextChar == '.') {
-                            throw new InputMismatchException("White space is required between PERIOD and NUMBER");
-                        }
-                        else {
-                            System.out.println( "Token: " + "NUMBER--> " + number + " Value: " +  29);
-                            in.unread(nextChar);
-                        }
-                    }
-                    else {
-                        String identOrReserved = "" + currChar;
-                        do {
-                            nextChar = (char)in.read();
-                            identOrReserved += nextChar;
+    public static boolean isOperatorSeperator (String key) {
 
-                        } while (nextChar != ' ' && nextChar != '\n' && !identOrReserved.matches(".*" + operatorsSeperatorsRegEx + ".*"));
-                        String result = identOrReserved.split(operatorsSeperatorsRegEx)[0];
+        return operatorSeperatorTokens.containsKey(key);
+    }
 
-                        if (reservedTokens.containsKey(result.toUpperCase())){
-                            System.out.println( "Token: " + "RESERVED--> " + result + " Value: " +  reservedTokens.get(result));
-                        }
-                        else {
-                            System.out.println( "Token: " + "IDENTIFIER--> " + result + " Value: " + 28);
-                        }
-                        in.unread(identOrReserved.substring(result.length(), identOrReserved.length()).toCharArray());
-                    }
-                    getToken();
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading from standard in. Exiting ... " + e.getMessage());
+    private static String transformKey (String key) {
+        if (key.equals("\n")) {
+            return "EOLN";
+        }
+        else if (key.equals(" ")) {
+            return "SPACE";
         }
 
+        else {
+            return key;
+        }
     }
-    
-    public static boolean isNumber (int c) {
+
+    public static void getToken(char currChar) {
+        char nextChar = readIn();
+        String key;
+        if (currChar == '.') {
+            if (isNumber(nextChar)) {
+                print(handleNumber(readIn(), "" + currChar + nextChar, true, true));
+            }
+            else {
+                unreadIn(nextChar);
+                print("Token: " + '.' + " Value: " + 31);
+            }
+        }
+        else if (isNumber(currChar)) {
+            print(handleNumber(nextChar, "" + currChar, false, true));
+        }
+        else if (currChar == '"') {
+            print(handleString(nextChar, "" + currChar));
+        }
+        else if (isOperatorSeperator(key = "" + currChar + nextChar)) {
+            print("Token: " + key + " Value: " + operatorSeperatorTokens.get(key));
+        }
+        else if (isOperatorSeperator(key = "" + currChar)) {
+            unreadIn(nextChar);
+            print("Token: " + transformKey(key) + " Value: " + operatorSeperatorTokens.get(key));
+        }
+        else {
+            print(handleReserveredOrIdentifier("" + currChar + nextChar));
+        }
+    }
+
+    private static String handleReserveredOrIdentifier(String string) {
+        if (string.matches(".*" + operatorsSeperatorsRegEx + ".*")) {
+            //Split the string so that the first item in the array will contain the reserved word or identifier
+            String result = string.split(operatorsSeperatorsRegEx)[0];
+            //Put separating character(s) back on input stack
+            unreadIn(string.substring(result.length(), string.length()).toCharArray()); // Really only pushing back one character but reliant on regex match so ...
+            if (reservedTokens.containsKey(result.toUpperCase())) {
+                return "Token: " + "RESERVED--> " + result + " Value: " +  reservedTokens.get(result);
+            }
+            else {
+                return "Token: " + "IDENTIFIER--> " + result + " Value: " + 28;
+            }
+        }
+
+        return handleReserveredOrIdentifier(string + readIn());
+    }
+
+    private static String handleNumber (char currChar, String number, boolean isFloatingPoint, boolean validNumber) throws InputMismatchException {
+        if (currChar == '.') {
+            if (isFloatingPoint) {
+                throw new InputMismatchException("White space is required between PERIOD and NUMBER");
+            }
+            else {
+                // Set validNumber to false as a digit must follow the decimal to still be a number
+                return handleNumber(readIn(), number + currChar, true, false);
+            }
+        }
+        if (!(isNumber(currChar))) {
+            if (!validNumber) {
+                throw new InputMismatchException("White space is required between PERIOD and NUMBER. No digits following the decimal place");
+            }
+            unreadIn(currChar);
+            return "Token: " + "NUMBER--> " + number + " Value: " +  29;
+        }
+        // Set validNumber to true as we have encountered at least one digit after the decimal place 
+        return handleNumber(readIn(), number + currChar, isFloatingPoint, true);
+    }
+
+    private static String handleString (char currChar, String string) {
+        if (currChar == '"') {
+            return "Token: " + "STRING--> " + string + currChar + " Value: " +  30;
+        }
+        return handleString(readIn(), string + currChar);
+    }
+
+    private static void print (String msg) {
+        System.out.println(msg);
+    }
+
+    private static char readIn() {
+        char input = '\uFFFF';
+        try {
+            input=(char)in.read();
+        } catch (IOException e) {
+            print("Error reading from standard in " + e.getMessage());
+            System.exit(-1);
+        }
+        return input;
+    }
+
+    private static void unreadIn(char [] buff) {
+        try {
+            in.unread(buff);
+        } catch (IOException e) {
+            print("Error pushing characters back to standard in " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    private static void unreadIn(char c) {
+        try {
+            in.unread(c);
+        } catch (IOException e) {
+            print("Error pushing characters back to standard in " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    private static boolean isNumber (int c) {
         return c >= '0' && c <= '9';
     }
 }
